cbca31f2dbc47eaccdce8455fccc8eda6cf818e3
Jose Flavio da Silva Maia <makarovmaia@gmail.com>
2021-04-16 18:31:08 -0300

    Criando page home e de add e tirando configuraçao do firebase.

---
+++ poupan_pe/lib/view/home_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            floating: false,
+            title: Text("PoupanPE"),
+            expandedHeight: 200.0,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text("3000"),
+              centerTitle: true,
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        onPressed: () {},
+      ),
+    );
+  }
+}
